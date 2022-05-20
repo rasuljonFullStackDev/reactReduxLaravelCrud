ca8cdc0abb57b22f519ff586e4b377cc46eb990a
abdurasulFulstakDev <tursunboyevabdurasuldevolop@gmail.com>
2022-05-20 10:30:40 +0500

    loyiha oquvplatforma boladigan boldi

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
-import React from "react";
-import { BrowserRouter as Router,Routes,Route } from "react-router-dom";
-import Register from "./pages/Register";
-
-function App(){
-  <Router>
-    <Routes>
-       <Route path="/" element = {<Register/>} />
-    </Routes>
-  </Router>
+import React from 'react'
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Register from './pages/Register';
+import axios from 'axios';
+axios.defaults.baseURL = 'http://127.0.0.1:8000/';
+axios.defaults.headers.post['Content-Type'] = "aplication/json";
+axios.defaults.headers.post['Accept'] = "aplication/json";
+axios.defaults.withCredentials = true;
+axios.interceptors.request.use(function (config) {
+  const token = localStorage.getItem('token');
+  config.headers.Authorization = token ? `Bearer ${token}` : ''
+  return config
+});
+const App = () => {
+  return (
+    <div>
+        <Router>
+          <Routes>
+            <Route path='/' element={<Register/>}/>
+          </Routes>
+        </Router>
+    </div>
+  )
 }
 
-export default App;
+export default App
--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,61 +1,152 @@
+import { useState } from 'react';
+import axios from 'axios';
 import { Form, Input, Button, Checkbox } from 'antd';
-import { UserOutlined, LockOutlined } from '@ant-design/icons';
+import { UserOutlined, LockOutlined, PhoneOutlined } from '@ant-design/icons';
+import { useForm } from 'antd/lib/form/Form';
+import Swal from 'sweetalert2';
 
 const Register = () => {
+    const [auth, setAuth] = useState(false);
+    const [forget, setForget] = useState(false);
+    const onFinish = async (values) => {
+        if (auth) {
+            let res = await axios.post('api/login', values)
+                .catch((err) => {
+                    console.log(err);
+                })
+            if (res.status === 200 && res.data.token.trim() !== "") {
+                Swal.fire({
+                    title: 'Tabriklaymiz!',
+                    text: res.data.xabar,
+                    icon: 'success',
+                    confirmButtonText: 'Cool'
+                })
+            }
+        } else {
+            let res = await axios.post('api/register', values)
+
+                .catch((err) => {
+                    console.log(err);
+                })
+            if (res.status === 200 && res.data.token.trim() !== "") {
+                Swal.fire({
+                    title: 'Tabriklaymiz!',
+                    text: res.data.xabar,
+                    icon: 'success',
+                    confirmButtonText: 'Cool'
+                })
+            }
+        }
+    };
+    const [form] = Form.useForm();
+    const authFun = () => {
+        setAuth(!auth)
+        form.resetFields();
+    }
     return (
-        <>
-            <Form
-                name="normal_login"
-                className="login-form"
-                initialValues={{
-                    remember: true,
-                }}
-                onFinish={onFinish}
-            >
-                <Form.Item
-                    name="username"
-                    rules={[
-                        {
-                            required: true,
-                            message: 'Please input your Username!',
-                        },
-                    ]}
-                >
-                    <Input prefix={<UserOutlined className="site-form-item-icon" />} placeholder="Username" />
-                </Form.Item>
-                <Form.Item
-                    name="password"
-                    rules={[
-                        {
-                            required: true,
-                            message: 'Please input your Password!',
-                        },
-                    ]}
-                >
-                    <Input
-                        prefix={<LockOutlined className="site-form-item-icon" />}
-                        type="password"
-                        placeholder="Password"
-                    />
-                </Form.Item>
-                <Form.Item>
-                    <Form.Item name="remember" valuePropName="checked" noStyle>
-                        <Checkbox>Remember me</Checkbox>
-                    </Form.Item>
+        <div className='register'>
+            <div className="formChild" id="formChild">
 
-                    <a className="login-form-forgot" href="">
-                        Forgot password
-                    </a>
-                </Form.Item>
 
-                <Form.Item>
-                    <Button type="primary" htmlType="submit" className="login-form-button">
-                        Log in
-                    </Button>
-                    Or <a href="">register now!</a>
-                </Form.Item>
-            </Form>
-        </>
+                {forget ? "" : <>
+
+
+                    <h1> {auth ? "Login in" : "Register"}</h1>
+                    <Form
+                        name="normal_login"
+                        className="login-form"
+                        size='large'
+                        onFinish={onFinish}
+                        form={form}
+
+                    >
+                        <Form.Item
+                            name="username"
+                            rules={[
+                                {
+                                    required: true,
+                                    message: 'Please input your Username!',
+                                },
+                            ]}
+                            hasFeedback
+                        >
+                            <Input prefix={<UserOutlined className="site-form-item-icon" />} placeholder="Username" />
+                        </Form.Item>
+                        {
+                            auth ? null :
+                                <>
+                                    <Form.Item
+                                        name="email"
+                                        rules={[
+                                            {
+                                                required: true,
+                                                message: 'Please input your Email!',
+                                            },
+                                        ]}
+                                        hasFeedback
+                                    >
+                                        <Input
+                                            prefix={<LockOutlined className="site-form-item-icon" />}
+                                            type="email"
+                                            placeholder="email"
+                                        />
+                                    </Form.Item>
+                                    <Form.Item
+                                        name="phone"
+                                        rules={[
+                                            {
+                                                required: true,
+                                                message: 'Please input your phone!',
+                                            },
+                                        ]}
+                                        hasFeedback
+                                    >
+                                        <Input
+                                            prefix={<PhoneOutlined className="site-form-item-icon" />}
+                                            type="tel"
+                                            placeholder="phone"
+                                        />
+                                    </Form.Item>
+                                </>
+                        }
+
+                        <Form.Item
+                            name="password"
+                            rules={[
+                                {
+                                    required: true,
+                                    message: 'Please input your Password!',
+                                },
+                            ]}
+                            hasFeedback
+                        >
+                            <Input
+                                prefix={<LockOutlined className="site-form-item-icon" />}
+                                type="password"
+                                placeholder="Password"
+                            />
+                        </Form.Item>
+                        <Form.Item>
+                            <Form.Item name="remember" valuePropName="checked" noStyle hasFeedback>
+                                <Checkbox>Eslab Qolaymi</Checkbox>
+                            </Form.Item>
+                            <br />
+                            <a className="login-form-forgot" href="">
+                                Parolni Tiklash
+                            </a>
+                        </Form.Item>
+                        <Form.Item>
+                            <Button type="primary" htmlType="submit" className="login-form-button">
+                                {auth ? "Login in" : "Register"}
+                            </Button>
+                            <br />
+                            <Button type='link' onClick={authFun}>  {!auth ? "Login" : "Register"}</Button>
+                        </Form.Item>
+                    </Form>
+                </>
+                }
+            </div>
+        </div>
     )
 }
 
